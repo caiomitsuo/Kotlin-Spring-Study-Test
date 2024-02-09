161fc4f67e74f40be4bc6d1fcd7b6ca68e637383
caio <caio.lima@edu.unifil.br>
2024-02-08 21:23:54 -0300

    Start project

---
+++ Spring+Junit_theNewboston_course/src/main/kotlin/br/com/curso/springboot/thenewboston/ThenewbostonApplication.kt
@@ -0,0 +1,11 @@
+package br.com.curso.springboot.thenewboston
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThenewbostonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThenewbostonApplication>(*args)
+}
